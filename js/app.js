e8a9abaea314b07e6a0548d70b62d4f36bb74a34
Masnoon Junaid <masnoon20@gmail.com>
2018-10-14 22:44:14 +0530

    update class and method

--- js/app.js
+++ js/app.js
@@ -20,7 +20,7 @@ Enemy.prototype.update = function(dt) {
     if(this.x >= 505) {
         this.x = 0;
     }
-    this.testCollision();
+    this.checkCollision();
 };
 
 // Draw the enemy on the screen, required method for game
@@ -31,14 +31,32 @@ Enemy.prototype.render = function() {
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
-
-
+Enemy.prototype.checkCollision = function(){
+    if(player.y + 131 >= this.y + 90 &&
+        player.y + 73 <= this.y + 135 &&
+        player.x + 25 <= this.x + 88 &&
+        player.x + 76 >= this.x + 11) {
+        console.log('collision');
+    gameReset();
+    }
+    // body... 
+};
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
+ var Player = function(x,y, speed) {
+    this.x = x;
+    this.y = y;
+    this.speed = speed;
+    this.sprite = 'images/char-boy.png';
+ };
 
-
-
+//updating method for player
+Player.prototype.update = function(){};
+//render the player    // body... 
+Player.prototype.render = function() {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+}; 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
 document.addEventListener('keyup', function(e) {
